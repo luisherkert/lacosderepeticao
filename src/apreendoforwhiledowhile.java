9c89a558bccacd1cb19acc445351311e0ea8ae03
Usuario <luis40105@edu.unipar.edu>
2024-09-11 19:47:01 -0300

    aula 1

---
+++ src/apreendoforwhiledowhile.java
@@ -0,0 +1,13 @@
+public class apreendoforwhiledowhile {
+
+    public static void main(String[] args) {
+
+        //inicialização; condição; incremento
+        // ++ -> vai somar + 1 i = i + 1
+        for ( int i = 0; i < 10; i++ ){
+            System.out.println("informe o nome do player 1:"+ (i+1));
+
+
+        }
+    }
+}
